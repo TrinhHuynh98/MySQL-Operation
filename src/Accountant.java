084a1687bbe6cfea536f01210c996f5a9ec24635
TrinhHuynh98 <47219486+TrinhHuynh98@users.noreply.github.com>
2019-06-22 13:50:16 +0700

    Add project mySQL operation

---
+++ src/Accountant.java
@@ -0,0 +1,50 @@
+
+public class Accountant {
+
+	private int id;
+	private String name, password, email, contactno;
+	//public Accountant(String name2, String password2, String email2, String contactno2) {
+		
+//	}
+	public Accountant() {}
+	public Accountant( String name, String password, String email, String contactno) {
+		super();
+		
+		this.name = name;
+		this.password = password;
+		this.email = email;
+		this.contactno = contactno;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getContactno() {
+		return contactno;
+	}
+	public void setContactno(String contactno) {
+		this.contactno = contactno;
+	}
+	
+	
+}
